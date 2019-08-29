3f1133cf6900f5624e8b0e9c344022462c878546
Aleksandr <olexander.o.havrylenko@gmail.com>
2019-08-29 11:33:10 +0300

    Updates

---
+++ precache-manifest.c50e180de477ba1767074af164d85f04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ca79d4fbf3b0a9aa2834472aa34187",
+    "url": "/firstreact/index.html"
+  },
+  {
+    "revision": "a9a5a1d37449029c099b",
+    "url": "/firstreact/static/css/main.68aa23f2.chunk.css"
+  },
+  {
+    "revision": "06a166ef2d6a68ced516",
+    "url": "/firstreact/static/js/2.f79769fb.chunk.js"
+  },
+  {
+    "revision": "a9a5a1d37449029c099b",
+    "url": "/firstreact/static/js/main.37ffe739.chunk.js"
+  },
+  {
+    "revision": "67c8a80763eb647aa0ad",
+    "url": "/firstreact/static/js/runtime~main.b5967ec7.js"
+  }
+]);
